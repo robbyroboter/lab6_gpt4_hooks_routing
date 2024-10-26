446993e8af16403caf5d74e32b19a78f74cbf703
robbyroboter <rosticzuev2002@mail.ru>
2024-10-26 18:05:07 +0500

    Первая отправка

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Brands from "../components/Brands";
+import WhatIsChatGpt from "../components/WhatIsChatGpt";
+import FutureHere from "../components/FutureHere";
+import Vr from "../components/Vr";
+import Purple from "../components/Purple";
+import HappensBlog from "../components/HappensBlog";
+import Footer from "../components/Footer";
+
+const Home = ({ isEditable }) => {
+  return (
+      <>
+          <section
+              className="section header"
+              id="header"
+              // contentEditable={isEditable}
+          >
+              <Header/>
+          </section>
+          <section className="section hero_section" id="hero">
+              <Hero/>
+          </section>
+          <section className="section brands_section" id="brands">
+              <Brands/>
+          </section>
+          <section className="section what_is_chatgpt_section" id="what-is-gpt">
+              <WhatIsChatGpt/>
+          </section>
+          <section className="section future_here" id="future">
+              <FutureHere/>
+          </section>
+          <section className="section vr" id="vr">
+              <Vr/>
+          </section>
+          <section className="section purple" id="purple">
+              <Purple/>
+          </section>
+          <section className="section happens_blog" id="happens">
+              <HappensBlog/>
+          </section>
+          <section className="section footer" id="footer">
+              <Footer/>
+          </section>
+      </>
+  );
+};
+
+export default Home;
